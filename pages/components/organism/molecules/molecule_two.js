61e7defd90aa1912b7d46eb708e12d3218273075
Marcos Aguilera Ely <maguilera@rochester.edu.co>
2022-02-19 00:31:37 -0500

    Added: new molecules
    - one and two molecule added

---
+++ pages/components/organism/molecules/molecule_two.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function molecule_two(props) {
+    return (
+        {/* <div className='content__menu'>
+            <h3>Filter</h3>
+            <input type="radio" id="1" name="fav_language" value="1" />
+            <label htmlFor="html">Our project</label>
+            <br />
+            <input type="radio" id="2" name="fav_language" value="2" />
+            <label htmlFor="css">Area description</label>
+            <br />
+            <input type="radio" id="3" name="fav_language" value="3" />
+            <label htmlFor="javascript">Preschool</label>
+            <br />
+            <input type="radio" id="4" name="fav_language" value="4" />
+            <label htmlFor="javascript">Lower Elementary</label>
+            <br />
+            <input type="radio" id="5" name="fav_language" value="5" />
+            <label htmlFor="javascript">Upper Elementary</label>
+            <br />
+            <input type="radio" id="6" name="fav_language" value="6" />
+            <label htmlFor="javascript">Middle School</label>
+            <br />
+            <input type="radio" id="7" name="fav_language" value="7" />
+            <label htmlFor="javascript">High School</label>
+        </div> */}
+    );
+}
+
+export default molecule_two;
