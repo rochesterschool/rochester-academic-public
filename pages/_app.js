943a3dbba07326ecd3cadf34c73ba851731032f0
Marcos Aguilera Ely <maguilera@rochester.edu.co>
2022-02-19 00:28:16 -0500

    Update: Nextjs UI framework has been implanted

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import { NextUIProvider } from '@nextui-org/react';
+import { Provider } from 'react-redux'
+//import { useStore } from '../store'
+
+
 import '../styles/globals.css'
 import styles from '../styles/Home.catalogue.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
